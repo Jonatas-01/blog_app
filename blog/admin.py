01a51246cadefa4294912c38ae21db2e6b56168d
JonatasM <jonatasmc02@gmail.com>
2025-02-09 19:45:37 +0000

    Create Comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+# The dot in front of models indicates that we are importing Post from a file named models
+# When we create a custom model and we want it to appear in the admin site
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -21,3 +21,26 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    # The primary purpose of the __str__ method is to provide a human-readable representation of the Model instance
+    def __str__(self):
+        return f"Post title: {self.title}. By: {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment: {self.body} by {self.author}"
