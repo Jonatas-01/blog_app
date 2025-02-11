c37a2ffb28b60b69b4d551117ccf78a3f7109ecb
JonatasM <jonatasmc02@gmail.com>
2025-02-11 11:33:19 +0000

    add summernote for more stylization to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 # The dot in front of models indicates that we are importing Post from a file named models
 # When we create a custom model and we want it to appear in the admin site
 
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
