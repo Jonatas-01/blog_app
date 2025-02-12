ed5ae6d92a39f97d1dcee32d804fb1c8d7a5698a
JonatasM <jonatasmc02@gmail.com>
2025-02-12 12:10:03 +0000

    add button to go to previus page in index html

--- blog/views.py
+++ blog/views.py
@@ -8,4 +8,5 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=True)
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 6  # control how many posts will appears per page
+    # When we set the paginate_by property in the ListView class, Django adds two items to the data that is passed through to the template: a boolean value, is_paginated and a dictionary object called page_obj, which we just call the Page object.
