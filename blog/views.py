aa8551f20697c4e925b98e8d42106469e7f72d98
JonatasM <jonatasmc02@gmail.com>
2025-02-14 20:19:38 +0000

    Add post_details view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -10,3 +10,25 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6  # control how many posts will appears per page
     # When we set the paginate_by property in the ListView class, Django adds two items to the data that is passed through to the template: a boolean value, is_paginated and a dictionary object called page_obj, which we just call the Page object.
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instace of :model:`blog.Post`.
+    
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
