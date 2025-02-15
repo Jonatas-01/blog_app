bc14d2abe0431dd117c4bd24c57f698db41d9768
JonatasM <jonatasmc02@gmail.com>
2025-02-15 16:50:47 +0000

    add comments to better understanding later on

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug_url>/', views.post_detail, name='post_detail'), # first slug is a data type like "int", second is the data value from the previus page
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,12 +6,12 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=True)
+    queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6  # control how many posts will appears per page
     # When we set the paginate_by property in the ListView class, Django adds two items to the data that is passed through to the template: a boolean value, is_paginated and a dictionary object called page_obj, which we just call the Page object.
 
-def post_detail(request, slug):
+def post_detail(request, slug_url): # slug is a url parameter
     """
     Display an individual :model:`blog.Post`.
 
@@ -24,11 +24,11 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+    queryset = Post.objects.filter(status=1) 
+    post = get_object_or_404(queryset, slug=slug_url) # first slug is a data set in post database, and we are setting the value of the parameter passed
 
     return render(
         request,
-        "blog/post_detail.html",
-        {"post": post},
+        "blog/post_detail.html", # template name
+        {"post": post}, # context: is the variable we will use in the template to display values
     )
