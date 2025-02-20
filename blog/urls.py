500b96113f26c61129b8db019557cfddf304a59e
JonatasM <jonatasmc02@gmail.com>
2025-02-20 12:20:46 +0000

    added comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug_url>/', views.post_detail, name='post_detail'), # first slug is a data type like "int", second is the data value from the previus page
+    # first slug is a data type like "int", second is the data value from the previus page
+    path('<slug:slug_url>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import CommentForm
 
 # Create your views here.
@@ -30,22 +31,25 @@ def post_detail(request, slug_url):  # slug is a url parameter
     queryset = Post.objects.filter(status=1)
     # first slug is a data set in post database, and we are setting the value of the parameter passed
     post = get_object_or_404(queryset, slug=slug_url)
-    comments = post.comments.all().order_by("-created_on") # it will return all comments related to the selected post by using related_name="comments"
-    comment_count = post.comments.filter(approved=True).count() # count of approved comments
-    
+    # it will return all comments related to the selected post by using related_name="comments"
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(
+        approved=True).count()  # count of approved comments
+
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            comment = comment_form.save(commit=False) # we put it as false because we are going to populate author and post first, then we save
-            comment.author = request.user #  the uyser current logged in
-            comment.post = post # post is the post we took from get_object_or_404 earlier on
+            # we put it as false because we are going to populate author and post first, then we save
+            comment = comment_form.save(commit=False)
+            comment.author = request.user  # the uyser current logged in
+            comment.post = post  # post is the post we took from get_object_or_404 earlier on
             comment.save()
             messages.add_message(
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
             )
-    
-    comment_form = CommentForm() # this line makes the form blank
+
+    comment_form = CommentForm()  # this line makes the form blank
 
     return render(
         request,
@@ -55,5 +59,47 @@ def post_detail(request, slug_url):  # slug is a url parameter
             "comments": comments,
             "comment_count": comment_count,
             "comment_form": comment_form,
-         },  # all contexts: is the variable we will use in the template to display values
+        },  # all contexts: is the variable we will use in the template to display values
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
